894a05859fb5b8911a28cb1a947219a0e31dcd44
Ken Goldfarb <hello@kengoldfarb.com>
2014-10-07 01:01:10 -0600

    add commonjs pattern

--- dist/parsley.js
+++ dist/parsley.js
@@ -2,18 +2,24 @@
 * Parsleyjs
 * Guillaume Potier - <guillaume@wisembly.com>
 * Version 2.0.5 - built Thu Aug 28 2014 11:27:36
+*
+* EDITED to add CommonJS pattern
+*
 * MIT Licensed
 *
 */
-!(function (factory) {
-  if (typeof define === 'function' && define.amd) {
-    // AMD. Register as an anonymous module depending on jQuery.
-    define(['jquery'], factory);
-  } else {
-    // No AMD. Register plugin with global jQuery object.
-    factory(jQuery);
-  }
-}(function ($) {
+!(function(factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD
+        define(['jquery'], factory);
+    } else if (typeof exports === 'object') {
+        // CommonJS
+        factory(require('jquery'));
+    } else {
+        // Browser globals
+        factory(jQuery);
+    }
+}(function($) {
   // small hack for requirejs if jquery is loaded through map and not path
   // see http://requirejs.org/docs/jquery.html
   if ('undefined' === typeof $ && 'undefined' !== typeof window.jQuery)
@@ -810,7 +816,7 @@ var Validator = ( function ( ) {
   // https://developer.mozilla.org/en-US/docs/JavaScript/Reference/Global_Objects/Array/indexOf
   if (!Array.prototype.indexOf)
     Array.prototype.indexOf = function (searchElement /*, fromIndex */ ) {
-        
+
         if (this === null) {
             throw new TypeError();
         }
@@ -861,7 +867,7 @@ var Validator = ( function ( ) {
     window[ 'undefined' !== typeof validatorjs_ns ? validatorjs_ns : 'Validator' ] = exports;
   }
 
-  return exports; 
+  return exports;
 } )( );
 
   // This is needed for Browserify usage that requires Validator.js through module.exports
